22d3a1ef19d0a4f86f37c4086ddb8a0edd9f3fa0
molezinif <gabrielmolezinif@gmail.com>
2023-12-27 21:51:36 -0300

    solving timeConversion

---
+++ easy/timeConversion.js
@@ -0,0 +1,43 @@
+function leadingZero(n) {
+  if (n < 10) {
+    return `0${n}`
+  }
+  return n
+}
+
+function timeConversion(s) {
+  const isPM = s.match('PM')
+
+  const hour = Number(s.substr(0, 2))
+
+  const minutes = Number(s.substr(3, 2))
+
+  const seconds = Number(s.substr(6, 2))
+
+  if (hour > 12 || minutes > 59 || seconds > 59) {
+    return 'Nice try'
+  }
+
+  if (isPM?.length) {
+    if (hour === 12) {
+      return `12:${leadingZero(minutes)}:${leadingZero(seconds)}`
+    }
+
+    return `${leadingZero(minutes)}:${leadingZero(minutes)}:${leadingZero(
+      seconds
+    )}`
+  }
+
+  if (hour === 12) {
+    return `00:${leadingZero(minutes)}:${leadingZero(seconds)}`
+  }
+
+  return s.replace('AM', '')
+}
+
+function main() {
+  const res = timeConversion('12:45:54PM')
+  console.log(res)
+}
+
+main()
